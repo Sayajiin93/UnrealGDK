2433d656eccba78a5049d98a077113564a5b861e
samiwh <sami@improbable.io>
2020-07-02 00:59:34 +0100

    Refactored OpUtils to use return values instead of out params.

--- SpatialGDK/Source/SpatialGDK/Public/Utils/OpUtils.h
+++ SpatialGDK/Source/SpatialGDK/Public/Utils/OpUtils.h
@@ -2,13 +2,17 @@
 
 #pragma once
 
+#include "SpatialView/OpList/OpList.h"
+
 #include "CoreMinimal.h"
 
 #include <WorkerSDK/improbable/c_worker.h>
 
 namespace SpatialGDK
 {
-void FindFirstOpOfType(const TArray<Worker_OpList*>& InOpLists, const Worker_OpType OpType, Worker_Op** OutOp);
-void FindFirstOpOfTypeForComponent(const TArray<Worker_OpList*>& InOpLists, const Worker_OpType OpType, const Worker_ComponentId ComponentId, Worker_Op** OutOp);
+
+Worker_Op* FindFirstOpOfType(const TArray<SpatialGDK::OpList>& InOpLists, const Worker_OpType OpType);
+Worker_Op* FindFirstOpOfTypeForComponent(const TArray<SpatialGDK::OpList>& InOpLists, const Worker_OpType OpType, const Worker_ComponentId ComponentId);
 Worker_ComponentId GetComponentId(const Worker_Op* Op);
+
 } // namespace SpatialGDK
